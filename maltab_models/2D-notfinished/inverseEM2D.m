add8e3cf728cdd9b84c5b8266e5b3c7676e66aba
Bart Anhorn <barth.anhorn@icloud.com>
2021-04-24 10:39:42 +0200

    update 2D

--- maltab_models/2D-notfinished/inverseEM2D.m
+++ maltab_models/2D-notfinished/inverseEM2D.m
@@ -6,13 +6,20 @@ tic
 % load the dataset
 load data2D
 
-xlog = 0:0.1:10; %
+xlog = 0:0.2:10; %
 
 % model parameters
 ndata = size(data,1);
-ztop = 0:0.1:4;
+ztop = 0:0.1:5;
 nlay = length(ztop);
 sigma_a = data(:, 1);
+nsteps = length(unique(data(:, 4)));
+
+% noise
+for i = 1:length(sigma_a)
+    sigma_a(i) = sigma_a(i)+normrnd(0,1)*5e-6;
+end
+data(:, 1) = sigma_a;
 
 ndata_p = size(data(data(:, 4) == 1), 1);
 ndata = size(data,1);
@@ -20,9 +27,9 @@ nlay = length(ztop);
 
 G_stack = cell(1, nlay);
 
-for j = 1:
+for j = 1:nsteps
     G_prov = zeros(ndata_p, nlay);   % initialize W matrix
-    for i=1:size(G_prov, 1)        % populate the matrix row-by-row
+    for i=1:size(G_prov, 1)          % populate the matrix row-by-row
         R = WeightEM(ztop, data(i,2), data(i,3));
         G_prov(i, :) = R;
     end
@@ -39,7 +46,7 @@ A = G'*G;
 b = G'*data(:, 1);
 
 m = cgs(A, b, 1e-10, 1e2);
-
+m = reshape(m, [], nsteps);
 
 toc
 figure()
