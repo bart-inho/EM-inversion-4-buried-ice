8a20216b39a04e7267db1bf7a9072a39b1d28bb4
Bart Anhorn <barth.anhorn@icloud.com>
2021-04-28 09:58:29 +0200

    upgrade regu

--- maltab_models/2D-notfinished/smoothweightEM2D.m
+++ maltab_models/2D-notfinished/smoothweightEM2D.m
@@ -1,4 +1,4 @@
-function [Dz,Dx] = smoothweightEM2D(nx,nz)
+function [Dx,Dz] = smoothweightEM2D(nx,nz)
 %
 % This function computes matrices Wx and Wz that approximate the horizontal
 % and vertical second derivatives when applied to a model vector m, where m
@@ -15,9 +15,9 @@ function [Dz,Dx] = smoothweightEM2D(nx,nz)
 
 ncells = nx*nz;
 weights = repmat([1 -2 1],ncells,1);
-Dz = spdiags(weights,[-1 0 1],ncells,ncells);
-Dz(1:nx:end,:) = 0;
-Dz(nx:nx:end,:) = 0;
 Dx = spdiags(weights,[-nx 0 nx],ncells,ncells);
 Dx(1:nx,:) = 0;
 Dx(((nz-1)*nx+1):nx*nz,:) = 0;
+Dz = spdiags(weights,[-1 0 1],ncells,ncells);
+Dz(1:nx:end,:) = 0;
+Dz(nx:nx:end,:) = 0;
